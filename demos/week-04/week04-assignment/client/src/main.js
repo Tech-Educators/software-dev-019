2d74df10ef3b9d3b6f3479d5fd8b14184cbef204
Manuel <manugonzalez.tech@gmail.com>
2025-05-22 13:03:07 +0100

    feat: add week 4 assignmenttemplate

---
+++ demos/week-04/week04-assignment/client/src/main.js
@@ -0,0 +1,17 @@
+//TODO: collect users data and send it to the server
+//- add interactivity to your data collection element (event)
+//- make sure your event handler fetches the server route that creates new data in the database
+// fetch("url",{
+//   method: "POST",
+//   headers: {
+
+//   },
+//   body:
+// })
+
+//! - you will be fetching your local host during development. When your project is finished and deployed, make sure you swap your local host urls with the server deployment url
+//http://localhost:8080/staff --> dev
+//https://deployedwebsiteurl.onrender.com/staff --> production
+
+//TODO: create DOM elements to render users' data
+// - make sure you use DOM manipulation methods
---
+++ demos/week-04/week04-assignment/server/queries.sql
@@ -0,0 +1,5 @@
+-- this is for you to add your SQL queries that you have used in in the SQL editor on Supabase
+
+-- query to create table
+-- query to insert data
+-- query to select data
